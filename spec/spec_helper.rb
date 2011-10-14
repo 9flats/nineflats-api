2b07a535feb77896c2e5271c3c5dafba8a0267d3
Thorsten Böttger <alto@mt7.de>
2011-10-14 20:07:40 +0200

    make sure no request is actually sent to the real API

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,6 @@ Dir["spec/fixtures/*.rb"].each {|f| require File.expand_path(f) }
 RSpec.configure do |config|
   # 
 end
+
+# make sure no request is sent to the API
+FakeWeb.allow_net_connect = false
