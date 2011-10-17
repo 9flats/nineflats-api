f756da66ee8880f195cf193ab4ef8ba32c0d0c7e
Lena Herrmann <lena@zeromail.org>
2011-10-17 15:54:07 +0200

    adding user favorites

--- lib/nineflats-api/user.rb
+++ lib/nineflats-api/user.rb
@@ -1,6 +1,7 @@
 module Nineflats
   class User < Base
-    attr_accessor :name, :slug, :user_photo_url, :self_url, :favorites_url, :full_url
+    attr_accessor :name, :slug, :user_photo_url, :favorites,
+                  :self_url, :favorites_url, :full_url
     
     def initialize(json)
       user = json.first[1]
@@ -15,13 +16,25 @@ module Nineflats
         @favorites_url = Nineflats::Base.object_link("favorites", user["links"])
       end
     end
-
-    def self.fetch(slug, lang)
-      User.new(Helpers.get_data(User.api_call(slug, lang)))
-    end
     
-    def self.api_call(slug, lang)
-      base_url + "/users/#{slug}?client_id=#{Nineflats::Base.client_app_key}&lang=#{lang}"
+    def favorites
+      return @favorites if @favorites
+      
+      json = Helpers.get_data(favorites_url)
+      
+      if json && json["places"]
+        @favorites = json["places"].collect do |place_hash|
+          Place.new(place_hash)
+        end
+      end
+    end
+
+    def self.fetch(slug)
+      User.new(Helpers.get_data(User.api_call(slug)))
+    end
+        
+    def self.api_call(slug)
+      base_url + "/users/#{slug}?client_id=#{Nineflats::Base.client_app_key}"
     end
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,14 +4,14 @@ describe Nineflats::User do
   before(:each) do
     Nineflats::Base.stub!(:client_app_key).and_return("WfKWrPywnEbMhlifGlrsLu2ULfvTwxrKQji5eg0S")
     FakeWeb.register_uri(:get, 
-      "http://api.9flats.com/api/v1/users/jana-k-1?client_id=#{Nineflats::Base.client_app_key}&lang=en", 
+      "http://api.9flats.com/api/v1/users/jana-k-1?client_id=#{Nineflats::Base.client_app_key}", 
       :body => Fixtures.user
     )
   end
   
   describe "fetch" do
     before(:each) do
-      @user = Nineflats::User.fetch('jana-k-1', 'en')
+      @user = Nineflats::User.fetch('jana-k-1')
     end
     
     it "should return a user" do
@@ -21,13 +21,58 @@ describe Nineflats::User do
     it "should set all the attributes" do
       @user.name.should == "Jana K."
       @user.slug.should == "jana-k-1"
-      @user.user_photo_url.should == "http://img2.9flats.com/users/photos/11405-1311181665/medium.jpg"
+      @user.user_photo_url.should == "http://img3.9flats.com/users/photos/11405-1311181665/medium.jpg"
     end
     
     it "should set the links" do
-      @user.self_url.should == "http://api.9flats.com/api/v1/users/jana-k-1"
+      @user.self_url.should == "http://api.9flats.com/api/v1/users/jana-k-1?client_id=WfKWrPywnEbMhlifGlrsLu2ULfvTwxrKQji5eg0S"
       @user.full_url.should == "http://www.9flats.com/users/jana-k-1"
-      @user.favorites_url.should == "http://api.9flats.com/api/v1/users/jana-k-1/favorites"
+      @user.favorites_url.should == "http://api.9flats.com/api/v1/users/jana-k-1/favorites?client_id=WfKWrPywnEbMhlifGlrsLu2ULfvTwxrKQji5eg0S"
+    end
+  end
+  
+  describe "favorites" do
+    before(:each) do
+      FakeWeb.register_uri(:get, 
+        "http://api.9flats.com/api/v1/users/jana-k-1/favorites?client_id=#{Nineflats::Base.client_app_key}", 
+        :body => Fixtures.user_favorites
+      )
+      @user = Nineflats::User.fetch('jana-k-1')
+    end
+    
+    it "should add the favorite places" do
+      @user.favorites.length.should == 7
+      
+      @user.favorites[0].class.should == Nineflats::Place
+      @user.favorites[0].name.should == "Maison de charme"
+      @user.favorites[0].city.should == "Olargues"
+      @user.favorites[1].slug.should == "55qm-loftwohnung"
+    end
+    
+    it "should return an empty array when there are no favorites" do
+      FakeWeb.register_uri(:get, 
+        "http://api.9flats.com/api/v1/users/jana-k-1/favorites?client_id=#{Nineflats::Base.client_app_key}", 
+        :body => '{"places":[]}'
+      )
+      
+      @user.favorites.should == []
+    end
+    
+    it "should return nil when the API call fails" do
+      FakeWeb.register_uri(:get, 
+        "http://api.9flats.com/api/v1/users/jana-k-1/favorites?client_id=#{Nineflats::Base.client_app_key}", 
+        :body => ''
+      )
+      
+      @user.favorites.should == nil
+    end
+    
+    it "should cache the reviews" do
+      Nineflats::Helpers.should_receive(:get_data).and_return(JSON.parse(Fixtures.user_favorites))
+      @user.favorites
+      
+      Nineflats::Helpers.should_not_receive(:get_data)
+      @user.favorites
     end
   end
 end
